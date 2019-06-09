64ad74d27b6c697021884c1e9b35993207d5cf39
Rolf Madsen <rolfrm@gmail.com>
2019-06-09 19:16:15 +0200

    added TouchingGround to KinematicBody2D

--- InputHandler.cs
+++ InputHandler.cs
@@ -9,13 +9,18 @@ public class InputHandler : Node2D
   public override void _Process(float delta)
   {
       var kin = this.GetParent() as KinematicBody2D;
+      if(kin.TouchingGround == false) return;
       float addedSpeed = 0.0f;
       if(Input.IsKeyPressed((int)KeyList.Left))
           addedSpeed -= 5;
       
       if(Input.IsKeyPressed((int)KeyList.Right))
           addedSpeed += 5;
-      float prev = kin.velocity.x;
+      if(Input.IsKeyPressed((int) KeyList.Space)){
+        kin.velocity.y = -100;
+        kin.TouchingGround = false;
+      }
+    float prev = kin.velocity.x;
     kin.velocity.x += addedSpeed;
     
   }
--- KinematicBody2D.cs
+++ KinematicBody2D.cs
@@ -15,7 +15,7 @@ public class KinematicBody2D : Godot.KinematicBody2D
 
     public Vector2 velocity = new Vector2(0,0);
     static Vector2 gravity = new Vector2(0,400);
-
+   public bool TouchingGround{get;set;}
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _PhysicsProcess(float delta)
   {
@@ -25,7 +25,15 @@ public class KinematicBody2D : Godot.KinematicBody2D
     var dist = info.Length();
     int slideCount = GetSlideCount();
     velocity = info;
+    TouchingGround = false;
     if(slideCount > 0){
+      for(int i = 0; i < slideCount; i++){
+        KinematicCollision2D col = GetSlideCollision(i);
+        if(col.Collider is StaticBody2D collider)
+        {
+            TouchingGround = true;
+        }
+      }
       
     }
 	velocity.x *= 0.99f;
