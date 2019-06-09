0012e4b5afdcb9ea4aca46a633956dba92e243c0
Rolf Madsen <rolfrm@gmail.com>
2019-06-09 23:03:30 +0200

    minor adjustment to jumping

--- InputHandler.cs
+++ InputHandler.cs
@@ -14,7 +14,10 @@ public class InputHandler : Node2D
   {
       var kin = this.GetParent() as KinematicBody2D;
       var foot = kin.GetNode("foot2") as foot2;
-      if(foot.IsTouching == false) return;
+      float MaxSpeed = this.MaxSpeed;
+      if(foot.IsTouching == false){
+        //MaxSpeed = 50;
+      }
       float addedSpeed = 0;
       if(Input.IsKeyPressed((int)KeyList.Left))
           addedSpeed += -1;
@@ -23,9 +26,9 @@ public class InputHandler : Node2D
           addedSpeed += 1;
 
       if(addedSpeed == 0){
-        addedSpeed = -Math.Sign(kin.velocity.x) * 0.8f;
+        kin.velocity.x *= 0.4f;
       }
-      if(Input.IsKeyPressed((int) KeyList.Space)){
+      if(Input.IsKeyPressed((int) KeyList.Space) && foot.IsTouching){
         kin.velocity.y = -JumpSpeed;
         kin.TouchingGround = false;
       }
